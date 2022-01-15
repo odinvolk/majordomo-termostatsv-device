826134c941b3b7015faeeda85c507af5acb67880
Паша <odinwolk@gmail.com>
2022-01-15 12:27:43 +0300

    Update SThermostatsv_valueUpdated.php

--- modules/devices/SThermostatsv_valueUpdated.php
+++ modules/devices/SThermostatsv_valueUpdated.php
@@ -1,5 +1,19 @@
 <?php
 
+if ($this->getProperty('alive') == 0) {
+ $this->setProperty('alive', 1);
+}
+$alive_timeout=(int)$this->getProperty('aliveTimeout')*60*60;
+if (!$alive_timeout) {
+    $alive_timeout=2*24*60*60; // 2 days alive timeout by default
+}
+setTimeout($ot.'_alive_timer', 'setGlobal("'.$ot.'.alive", 0);', $alive_timeout);
+
+$disabled = $this->getProperty('disabled');
+if ($disabled) {
+    return;
+}
+
 $status = $this->getProperty('status');
 $currentTemperature = $this->getProperty('value');
 $ncno = $this->getProperty('ncno');
